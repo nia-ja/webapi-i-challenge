4f490967dbcb3bffc0c64377e494aa5aee11b5df
Kasia Bondarava <kasia.bondarava@gmail.com>
2019-05-08 13:10:41 -0700

    Added delete user functionality

--- users/src/App.js
+++ users/src/App.js
@@ -9,16 +9,36 @@ class App extends Component {
     axios
       .get("http://localhost:5001/api/users")
       .then(response => {
-        console.log(response);
         this.setState({ users: response.data });
       })
       .catch(err => console.log(err));
+  }
+  deleteUser(id) {
+    axios
+      .delete(`http://localhost:5001/api/users/${id}`)
+      .then(response => {
+        const newUsers = this.state.users.filter(user => {
+          if(user.id === id) {
+            return null
+          } else {
+            return user
+          }
+        });
+        this.setState({ users: newUsers });
+      })
+      .catch(err => console.log(err));
+
   }
   render() {
     return (
       <div className="App">
         {this.state.users.map(user => {
-          return <p key={user.id}>{user.name}</p>
+          return (
+            <div className="user-wrapper" key={user.id}>
+              <p>{user.name}</p>
+              <button onClick={() => this.deleteUser(user.id)}>✘</button>
+            </div>
+          )
         })}
       </div>
     );
